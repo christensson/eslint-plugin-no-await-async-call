37b76dbc69744aae7875c27a2cbb8e33f7978fa4
christensson <marcus.christensson@gmail.com>
2016-12-18 21:48:08 +0100

    More work on function scopes

--- rules/no-await-async-call.js
+++ rules/no-await-async-call.js
@@ -10,7 +10,7 @@ module.exports = function(context) {
   const stack = [];
 
   // Push root-frame...
-  stack.push({ funcProps: {} });
+  stack.push({ level: 0, funcProps: {}, rememberedChildProps: {} });
 
   function setFuncProp(frame, funcId, propName, value) {
     const props = frame.funcProps;
@@ -20,27 +20,56 @@ module.exports = function(context) {
     props[funcId][propName] = value;
   }
 
-  function getFuncProp(funcId, propName) {
+  function isFuncAsync(funcId) {
     // Go through stack and search backwards...
     for (let i = stack.length - 1; i >= 0; --i) {
       const props = stack[i].funcProps;
       if (funcId in props) {
-        return props[funcId][propName];
+        return props[funcId][PROP.ASYNC];
+      }
+    }
+    return undefined;
+  }
+
+  function isFuncFailedIfAsync(funcId) {
+    const checkProps = function(props, funcId) {
+      if ((funcId in props) && (PROP.REPORT_IF_ASYNC in props[funcId])) {
+        const reportIfAsync = props[funcId][PROP.REPORT_IF_ASYNC];
+        if (reportIfAsync) {
+          // Remove from props...
+          delete props[funcId][PROP.REPORT_IF_ASYNC];
+        }
+        return reportIfAsync;
+      }
+      return undefined;
+    };
+
+    // Go through stack and search backwards...
+    for (let i = stack.length - 1; i >= 0; --i) {
+      const frame = stack[i];
+      let reportIfAsync = checkProps(frame.funcProps, funcId);
+      if (reportIfAsync === undefined) {
+        reportIfAsync = checkProps(frame.rememberedChildProps, funcId);
+      }
+      if (reportIfAsync !== undefined) {
+        return reportIfAsync;
       }
     }
     return undefined;
   }
 
   function onFunctionEnter(node) {
+    const callingFrame = stack[stack.length - 1];
       const frame = {
+        level: callingFrame.level + 1,
         isAsync: node.async,
         // Map to store if:
         // - PROP.ASYNC - Visited function declarations represent async functions
         // - PROP.REPORT_IF_ASYNC - Visited function calls within an async function
         // aren't awaited and thus shall report errors if later found out to be async.
-        funcProps: {}
+        funcProps: {},
+        rememberedChildProps: {}
       };
-      const callingFrame = stack[stack.length - 1];
       stack.push(frame);
       let funcName;
       if (node.type === "FunctionDeclaration") {
@@ -65,7 +94,7 @@ module.exports = function(context) {
       if (funcName) {
         setFuncProp(callingFrame, funcName, PROP.ASYNC, node.async || false);
         if (node.async) {
-          const reportNodeIfAsync = getFuncProp(funcName, PROP.REPORT_IF_ASYNC)
+          const reportNodeIfAsync = isFuncFailedIfAsync(funcName);
           if (reportNodeIfAsync) {
             context.report({
               node: reportNodeIfAsync,
@@ -78,6 +107,16 @@ module.exports = function(context) {
 
     function onFunctionExit(node) {
       const frame = stack.pop();
+      const callingFrame = stack[stack.length - 1];
+      // Check funcProps if we shall save any...
+      const propsToRemember = {};
+      for (const funcName of Object.keys(frame.funcProps)) {
+        const props = frame.funcProps[funcName];
+        if (PROP.REPORT_IF_ASYNC in props) {
+          propsToRemember[funcName] = props;
+        }
+      }
+      Object.assign(callingFrame.rememberedChildProps, frame.rememberedChildProps, propsToRemember);
     }
 
   return {
@@ -112,7 +151,7 @@ module.exports = function(context) {
         const calledFunctionName = node.callee.name;
         if (!calledWithAwait) {
           // That is called without await...
-          const calledFuncIsAsync = getFuncProp(calledFunctionName, PROP.ASYNC);
+          const calledFuncIsAsync = isFuncAsync(calledFunctionName);
           if (calledFuncIsAsync === undefined) {
             const callingFrame = stack[stack.length - 2];
             // We don't know yet if called function is async...
--- test/no-await-async-call.js
+++ test/no-await-async-call.js
@@ -12,28 +12,49 @@ const errors = [{type: "CallExpression", message: MESSAGE}];
 ruleTester.run("no-await-async-call", rule, {
   valid: [
     "function f() {g()}",
-    "async function g() {await x}; async function f() {await g();}",
+    "async function g() {}; async function f() {await g()}",
   ],
 
   invalid: [
     // async func f calls async func g without await (g declared before f)
-    { code: "async function g() {await x}; async function f() {g();}", errors },
+    { code: "async function g() {}; async function f() {g()}", errors },
     // same, but g declared after f
-    { code: "async function f() {g();}; async function g() {await x}", errors },
+    { code: "async function f() {g()}; async function g() {}", errors },
     // async func f calls anonymous async func g without await (g declared before f)
-    { code: "let g; g = async function() {await x}; async function f() {g();}", errors },
-    { code: "let g = async function() {await x}; async function f() {g();}", errors },
+    { code: "let g; g = async function() {}; async function f() {g()}", errors },
+    { code: "let g = async function() {}; async function f() {g()}", errors },
     // Same with arrow functions...
-    { code: "let g; g = async () => {await x}; async function f() {g();}", errors },
-    { code: "let g = async () => {await x}; async function f() {g();}", errors },
-    { code: "let g; g = async function() {await x}; async () => {g();}", errors },
-    { code: "let g = async function() {await x}; async () => {g();}", errors },
-    { code: "let g; g = async () => {await x}; async () => {g();}", errors },
-    { code: "let g = async () => {await x}; async () => {g();}", errors },
+    { code: "let g; g = async () => {}; async function f() {g()}", errors },
+    { code: "let g = async () => {}; async function f() {g()}", errors },
+    { code: "let g; g = async function() {}; async () => {g()}", errors },
+    { code: "let g = async function() {}; async () => {g()}", errors },
+    { code: "let g; g = async () => {}; async () => {g()}", errors },
+    { code: "let g = async () => {}; async () => {g()}", errors },
     // Nested functions...
-    { code: "function parent() { let g = async () => {await x}; async () => {g();} }", errors },
-    { code: "let g = async () => {await x}; function parent() { async () => {g();} }", errors },
-    { code: "let g = async () => {await x}; function parent1() { function parent2() {async () => {g();} } }", errors },
-    //{ code: "let g; function parent() { async () => {g();} }; g = async () => {await x}", errors },
+    { code: "function parent() { let g = async () => {}; async () => {g()} }", errors },
+    { code: "let g = async () => {}; function p() { async () => {g()} }", errors },
+    { code: "let g = async () => {}; function p1() { function p2() {async () => {g()} } }", errors },
+    { code: `let g;
+      function p() {
+        async () => {
+          g();
+        }
+      };
+      g = async () => {await x}`,
+      errors
+    },
+    { code: `let g1; let g2;
+      function p1() {
+        async function p2() {
+          async () => {
+            g2();
+            g1();
+          }
+        }
+      };
+      g1 = async () => {};
+      g2 = async () => {}`,
+      errors: [{type: "CallExpression", message: MESSAGE}, {type: "CallExpression", message: MESSAGE}]
+    },
   ]
 });
